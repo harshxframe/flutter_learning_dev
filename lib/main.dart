da53736aa95dfac2f213056c8e585b282f80dd22
scotedflotsin <scotedflotsin.co.ltd@gmail.com>
2025-07-07 23:56:12 +0530

    First commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,19 @@
 import 'package:flutter/material.dart';
 
+import 'home_page.dart';
+
 void main(){
   runApp(MyApp());
 }
 
 
-
 class MyApp extends StatelessWidget{
   const MyApp({super.key});
-
-
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Text("Hello Harsh Sir!"),
-        ),
-      )
+      home: HomePage(),
     );
   }
 
